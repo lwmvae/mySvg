72153c5d01faf49d4b5b1b3e06bacb60e752a884
lwmvae <lwmvae@163.com>
2018-04-03 16:02:29 +0800

    test

--- src/confirm/color - 副本/color.vue
+++ src/confirm/color - 副本/color.vue
@@ -1,8 +1,10 @@
 <template>
-  <popup title="可见动态" @save="saveValue" @del="del">
+  <popup title="颜色动态" @save="saveValue" @del="del">
     <el-form ref="form" :model="form" label-width="60px">
       <el-form-item label="变量名">
         <el-input v-model="form.name" class="name"></el-input>
+        <el-radio v-model="form.radio" label="1">背景色</el-radio>
+        <el-radio v-model="form.radio" label="2">文字颜色</el-radio>
       </el-form-item>
       <table class="table" v-show="form.value.length">
         <thead>
@@ -12,7 +14,9 @@
           <th>下限</th>
           <th></th>
           <th>上限</th>
-          <th>是否可见</th>
+          <th>闪烁</th>
+          <th>基本色</th>
+          <th>变换色</th>
         </thead>
         <tbody>
           <tr v-for="(item,index) in form.value">
@@ -26,23 +30,18 @@
               <i class="less"></i>
             </td>
             <td><el-input class="num" v-model="item.max"></el-input></td>
-            <td><el-select v-model="item.val" placeholder="请选择">
-              <el-option
-              v-for="option in options"
-              :key="option.value"
-              :label="option.label"
-              :value="option.value">
-            </el-option>
-          </el-select></td>
-          <el-button plain class="el-icon-minus" @click="reduce(index)"></el-button>
-        </tr>
-      </tbody>
-    </table>
-    <div class="btn-add">
-      <el-button type="primary" class="el-icon-plus" @click="add"></el-button>
-    </div>
-  </el-form>
-</popup>
+            <td><el-checkbox v-model="item.twinkle"></el-checkbox></td>
+            <td><el-color-picker v-model="item.color1"></el-color-picker></td>
+            <td><el-color-picker v-model="item.color2"></el-color-picker></td>
+            <el-button plain class="el-icon-minus" @click="reduce(index)"></el-button>
+          </tr>
+        </tbody>
+      </table>
+      <div class="btn-add">
+        <el-button type="primary" class="el-icon-plus" @click="add"></el-button>
+      </div>
+    </el-form>
+  </popup>
 </template>
 <script>
 import Popup from '../popup/popup'
@@ -52,20 +51,16 @@ export default {
     return {
       form: {
         name: null,
+        radio:'1',
         value:[{
           choose:true,
           min:null,
           max:null,
-          val:null
+          twinkle:false,
+          color1:'#409EFF',
+          color2:'#FF0000'
         }]
-      },
-      options:[{
-        value:'1',
-        label:'可见'
-      },{
-        value:'0',
-        label:'不可见'
-      }]
+      }
     }
   },
   methods:{
@@ -77,7 +72,7 @@ export default {
 
   },
   add(){
-    this.form.value.push({choose:true,min:null,max:null,val:null})
+    this.form.value.push({choose:true,min:null,max:null,twinkle:false,color1:'#409EFF',color2:'#FF0000'})
   },
   reduce(index){
     this.form.value.splice(index,1)
@@ -98,5 +93,5 @@ components: {
 }
 </script>
 <style>
-@import 'visual.css'
+@import 'color.css'
 </style>
